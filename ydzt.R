9630ede516d572ab52345082c134c3de12a96c29
jingql <jingqinlong@sina.com>
2017-08-25 16:41:13 +0800

    ydzt.R

--- ydzt.R
+++ ydzt.R
@@ -1,5 +1,5 @@
 ##### 景钦隆，2014年8月26日撰写，广州市疾控中心  ##### 
-### 2017年8月25日修订，case1[,7]--case1[,10]，增加此15天和上15天病例数两个变量
+### 2017年8月25日修订，case1[,7]--case1$发病日期，增加此15天和上15天病例数两个变量
 ### 此程序用于登革热疫点（以街道为空间统计单元）状态统计，可用于疫点状态信号探测
 ### 包括疫情始终日期、疫情持续时间、疫情波次、疫情状态（持续、结束）、疫情强度（暴发、散发）、疫情趋势（上升、下降、持平）
 ### 其中疫情状态、疫情强度、疫情趋势以15天（登革热最长潜伏期）为时间聚集性探测期限
@@ -8,7 +8,9 @@
 mdf <- read.csv(file=file.choose(),header=TRUE,as.is=TRUE)
 
 library(plyr)
-ydzt <- function(mdf=mdf,currentdate=Sys.Date()){
+library(reshape)
+
+ydzt <- function(mdf=mdf,currentdate=Sys.Date()){ #疫点状态统计函数
     setwd("D:/")
     mdf.cf <- mdf[duplicated(mdf$姓名),]
     ##### 查找重名病例 ##########
@@ -28,7 +30,7 @@ ydzt <- function(mdf=mdf,currentdate=Sys.Date()){
     ydzt.st$outbreak=ydzt.st$frequency=ydzt.st$trend=rep(0,length(count))
     for(i in 1:length(count)){
         case1=subset(mdf,街道==count[i])
-        case2=subset(case1,as.Date(case1$发病日期)>=(currentdate-15))
+        case2=subset(case1,as.Date(case1$网络报告时间)>=(currentdate-15)) #发病日期与网络报告时间变换
         ydzt.st$outbreak[i]=ifelse(nrow(case2)>=3,"暴发","散发")
         time1=case1$发病日期
         time2=sort(unique(as.Date(time1)))
@@ -36,7 +38,7 @@ ydzt <- function(mdf=mdf,currentdate=Sys.Date()){
         if (length(time3)>0) {for(j in 1:(length(time2)-1)){time3[j]=difftime(time2[j+1],time2[j])}}
         if ((length(time3)>0)==FALSE) {time3=1}
         ydzt.st$frequency[i]=length(time3[time3>=25])+1
-        case3=subset(case1,as.Date(case1$发病日期)>=(currentdate-30)&as.Date(case1$发病日期)<(currentdate-15))
+        case3=subset(case1,as.Date(case1$网络报告时间)>=(currentdate-30)&as.Date(case1$网络报告时间)<(currentdate-15)) # 发病日期与网络报告时间变换
         ydzt.st$trend[i]=ifelse(nrow(case2)>nrow(case3),"上升",ifelse(nrow(case2)<nrow(case3),"下降","持平"))
         ydzt.st$curr15days[i] = nrow(case2)
         ydzt.st$past15days[i] = nrow(case3)
@@ -55,6 +57,70 @@ ydzt <- function(mdf=mdf,currentdate=Sys.Date()){
     return(ydzt.st)
 }
 
+blrb <- function(mdf=mdf,today=Sys.Date()){ #病例日报统计函数
+    mdf$区县 <- substr(mdf$现住详细地址,1,3)
+    mdf$街道 <- substr(mdf$现住详细地址,4,6)
+    mdf$cases <- 1
+    hz <- aggregate(cases~区县+街道,length,data=mdf)
+    mdf.rq <- mdf[c("区县","街道","性别","职业","发病日期","网络报告时间","病例分类","报告单位","cases")]
+    mdf.rh <- melt(mdf.rq,id=c("区县","街道","性别","职业","发病日期","网络报告时间","病例分类","报告单位"))
+    hz.r <- cast(mdf.rh,区县+街道~病例分类,length)
+    # hz.r$累计病例 <- hz.r$临床诊断病例+hz.r$实验室确诊病例+hz.r$疑似病例
+    hz.r$累计病例 <- hz.r$确诊病例+hz.r$疑似病例
+    hz.r.sort <- hz.r[order(hz.r$区县,-hz.r$累计病例),]
+    hz.r.sort$当日新增病例 <- 0
+    #today <- as.Date(as.character(as.Date(Sys.Date(),"%Y-%m-%d")))
+    if ((today-1) %in% as.Date(mdf$网络报告时间)){
+      xz.mdf <- mdf[as.Date(mdf$网络报告时间)==(today-1),]
+      xz.hz <- aggregate(cases~区县+街道,length,data=xz.mdf)
+      for (i in 1:nrow(xz.hz)){
+       for (j in 1:nrow(hz.r.sort)){
+         if ((hz.r.sort$区县[j]==xz.hz$区县[i])&(hz.r.sort$街道[j]==xz.hz$街道[i])) hz.r.sort$当日新增病例[j]=xz.hz$cases[i]
+         }}
+    }
+    # qx.xz.hz <- aggregate(cbind(累计病例,当日新增病例,临床诊断病例,实验室确诊病例,疑似病例)~区县,sum,data=hz.r.sort)
+    # qx.xz.hz <- aggregate(cbind(累计病例,当日新增病例,临床诊断病例,实验室确诊病例,疑似病例)~区县,sum,data=hz.r.sort)
+    qx.xz.hz <- aggregate(cbind(累计病例,当日新增病例,确诊病例,疑似病例)~区县,sum,data=hz.r.sort)
+    qx.xz.hz <- aggregate(cbind(累计病例,当日新增病例,确诊病例,疑似病例)~区县,sum,data=hz.r.sort)
+    qx.xz.hz$街道 <- " "
+    final <- rbind(hz.r.sort,qx.xz.hz)
+    final.sort <- final[order(final$区县,-final$累计病例),c("区县","街道","累计病例","当日新增病例","确诊病例","疑似病例")]
+    colsum <- colSums(qx.xz.hz[,2:5])
+    colsum.df <- as.data.frame(t(colsum))
+    colsum.df$街道 <- " "
+    colsum.df$区县 <- "合计"
+    final.final <- rbind(final.sort,colsum.df)
+    write.csv(final.final,paste0("登革热日报表",today,".csv"),row.names = FALSE)
+    shell.exec(paste0("D:/","登革热日报表",today,".csv"))
+    return(final.final)
+}
+
+
+mrxz <- function(mdf=mdf,today=Sys.Date()){
+    #每日区县新增病例类型统计函数，程序待调整
+    xz.mdf <- mdf[as.Date(mdf$网络报告时间)==(today-2),]
+    xz.mdf.rq <- xz.mdf[c("区县","街道","性别","职业","发病日期","网络报告时间","病例分类","报告单位","cases")]
+    xz.mdf.melt <- melt(xz.mdf.rq,id=c("区县","街道","性别","职业","发病日期","网络报告时间","病例分类","报告单位"))
+    xz.hz.r <- cast(xz.mdf.melt,区县~病例分类,length)
+    # xz.hz.r$确诊 <- xz.hz.r$临床诊断病例+xz.hz.r$实验室确诊病例
+    xz.hz.r$确诊 <- xz.h z.r$确诊病例
+    names(xz.hz.r)[4]<- "疑似"
+    xz.qx.hz <- xz.hz.r[,c("区县","确诊","疑似")]
+    xz.qx.hz.colsum <- colSums(xz.qx.hz[2:3])
+    xz.qx.hz.colsum.df <- as.data.frame(t(xz.qx.hz.colsum))
+    xz.qx.hz.colsum.df$区县 <- "合计"
+    names(xz.qx.hz.colsum.df)[1:3] <- c("确诊","疑似","区县")
+    xz.qx.hz.final <- rbind(xz.qx.hz,xz.qx.hz.colsum.df)
+    write.csv(xz.qx.hz.final,paste0("区县每日新增病例分类",today,".csv"),row.names = FALSE)
+    shell.exec(paste0("D:/","区县每日新增病例分类",today,".csv"))
+}
+
 # 计算疫点状态表
 ydzt <- ydzt(mdf=mdf,currentdate=Sys.Date())
 ydzt
+
+blrb <- blrb(mdf=mdf,today=Sys.Date())
+blrb
+
+mrxz <- mrxz(mdf=mdf,today=Sys.Date())
+mrxz
